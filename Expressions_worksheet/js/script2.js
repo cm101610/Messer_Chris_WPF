77a05b4b8e1021db0d6e415ab146b08e023ceeaf
Chris Messer <cm101610@fullsail.edu>
2014-04-09 19:56:13 -0400

    Correction to Slice of Pie part 1
    Fixed console.log to show result as Each person ate X slices of pizza
    at the party.

--- Expressions_worksheet/js/script2.js
+++ Expressions_worksheet/js/script2.js
@@ -1,7 +1,8 @@
 var noSlices = 5.6;
 var noPeople = 11;
 var noPizza = 13;
-
+var string1 = "Each person ate "
+var string2 = " slices of pizza at the party."
 var result = (noPizza * noSlices)/ noPeople;
 
-console.log(result);
+console.log(string1 + result + string2);
