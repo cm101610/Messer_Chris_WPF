48651fb0f92d8687aa5eb68dc36276d433bc0fcc
Chris Messer <cm101610@fullsail.edu>
2014-04-09 19:49:28 -0400

    Update to Sparky exercise
    changed the console.log to represent answer as the sentence: Sparky is
    X human years old which is X in dog years.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,5 +3,8 @@
 var dogYears = 7;
 var humanYears = 4;
 var result = humanYears * dogYears;
+var string1 = "Sparky is ";
+var string2 = " human years old which is ";
+var string3 = " in dog years";
 
-console.log(result);
+console.log(string1 + result + string2 + dogYears + string3);
---
+++ Expressions_worksheet/js/script3.js
@@ -0,0 +1,7 @@
+var noSlices = 8;
+var noPeople = 10;
+var noPizza = 6;
+
+var result = (noPizza * noSlices)/ noPeople;
+
+console.log(result);
