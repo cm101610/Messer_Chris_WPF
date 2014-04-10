bf9db44e8133e458c3503c1a01b66d0f8c505ca0
Chris Messer <cm101610@fullsail.edu>
2014-04-10 18:49:53 -0400

    2nd Prompt
    Added second prompt looking for another Adjective.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -3,3 +3,9 @@
 //Entering data at prompts and then reading the Mad Lib back
 
 var madLib1 = prompt("Please type and Adjective in the box below.");
+
+//Ran script, prompt ok
+
+var madLib2 = prompt("Please type and Adjective in the box below.");
+
+//Ran script, prompt ok
