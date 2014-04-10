7025b2d618c19fd923faa36a196df1cae902d892
Chris Messer <cm101610@fullsail.edu>
2014-04-10 15:15:51 -0400

    Final changes
    Changed inches to feet and changed the cost per sq. foot to .02.  Also,
    moved the console.log(totalCost) ahead of the totalYard alert so that
    it appears at the same time as the alert.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,10 +1,10 @@
 //Beginning with var inputs
 
-var frontWidth = prompt("We are going to provide you an estimate for mowing your lawn.  \n\nPlease tell us the width of the longest portion of your front lawn, in inches.");
+var frontWidth = prompt("We are going to provide you an estimate for mowing your lawn.  \n\nPlease tell us the width of the longest portion of your front lawn, in feet.");
 
 //Tested and prompt works.
 
-var frontLength = prompt("Next, what is the length of the longest portion of your front lawn, also in inches?");
+var frontLength = prompt("Next, what is the length of the longest portion of your front lawn, also in feet?");
 
 //Tested and prompt works.
 
@@ -12,11 +12,11 @@ var frontYard = frontWidth * frontLength; //calculation works with console.log(f
 
 //console.log(frontYard);
 
-var backWidth = prompt("Now, please tell us the width of the longest portion of your back yard.");
+var backWidth = prompt("Now, please tell us the width of the longest portion of your back yard, again in feet.");
 
 //Tested and prompt works.
 
-var backLength = prompt("Thank you! \n\nFinally, please tell us the lenght of the longest portion of your back yard.");
+var backLength = prompt("Thank you! \n\nFinally, please tell us the lenght of the longest portion of your back yard, in feet.");
 
 //Tested and prompt works.
 
@@ -30,8 +30,10 @@ var totalYard = frontYard + backYard;
 
 //console.log(totalYard);
 
-var totalCost = totalYard * .25;
+var totalCost = totalYard * .02;
 
 //console.log(totalCost);
 
-var totalYard = prompt("The total area of your front yard is " + frontYard + " sq. in. and the total area of your back yard is " + backYard + " sq. in. \n\nFor a grandtotal of " + totalYard + " sq. in.\n\nThe total cost for mowing your lawn will be $" + totalCost);
+console.log(totalCost);
+
+var totalYard = alert("The total area of your front yard is " + frontYard + " sq. feet and the total area of your back yard is " + backYard + " sq. feet. \n\nFor a grand total of " + totalYard + " sq. feet.\n\nThe total cost for mowing your lawn will be $" + totalCost);
