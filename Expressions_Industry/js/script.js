832af317238a2700139fc265aa78959a93be68b0
Chris Messer <cm101610@fullsail.edu>
2014-04-10 16:30:48 -0400

    Added first prompt for Expressions_Industry
    Added the first prompt for visitors to site.

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,3 @@
+//Determining the Production Cost for a Client
+
+var voiceArray = prompt("So you wanna make a commercial for your business? \n\nWell you've come to the right place! \n\nHow much would you like to spend for Voice Over production in your commercial?");
