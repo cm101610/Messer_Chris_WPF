cf58bf7036976ef3bf57bc4bffbb74a05a2a7d65
Chris Messer <cm101610@fullsail.edu>
2014-04-10 14:36:49 -0400

    Third var added
    Added var for backWidth

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,4 +4,8 @@ var frontWidth = prompt("We are going to provide you an estimate for mowing your
 
 //Tested and prompt works.
 
-var frontLength = prompt("What is the length of the longest portion of your front lawn, also in inches?");
+var frontLength = prompt("Next, what is the length of the longest portion of your front lawn, also in inches?");
+
+//Tested and prompt works.
+
+var backWidth = prompt("Now, please tell us the width of the longest portion of your back yard.");
