0baa417f45dc9bbc57379c1ec4e347d2c3e887ed
Chris Messer <cm101610@fullsail.edu>
2014-04-10 16:35:49 -0400

    Beginning Array based on customer input
    Added the first array and tested with console.log to be sure it’s
    printing.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,10 @@
 //Determining the Production Cost for a Client
 
 var voiceArray = prompt("So you wanna make a commercial for your business? \n\nWell you've come to the right place! \n\nHow much would you like to spend for Voice Over production in your commercial?");
+
+//Prompt tested, working
+
+var myArray = new Array();
+myArray[0] = voiceArray;
+
+console.log(myArray[0]);
