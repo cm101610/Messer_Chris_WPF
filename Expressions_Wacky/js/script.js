7a5fb587fd3071eb4f71ab0ed02dce0091629e56
Chris Messer <cm101610@fullsail.edu>
2014-04-10 19:09:04 -0400

    Added the madLib story
    Added the madLib story, inputting each of the prompt results into their
    correct position in the story.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -60,3 +60,6 @@ var madLib14 = prompt("Please type and Verb Ending in -ING in the box below.");
 
 //Take the results of the prompts and string them together to complete the madLib14
 
+var wackyStory = alert("Good afternoon, ladies and gentlemen.  This is your favorite sportscaster bringing you the big football game between the Columbia University " + madLib12 + " and the West Point " + madLib13 + ". \n\nThe center has just snapped the " + madLib6 + " back to the Columbia star halfback, " + madLib3 + ", who is running around his own left " + madLib7 + ".  \n\nThere he's tackled hard around the " + madLib11 + ".  \n\nNow it's West Point's ball and " + madLib10 + " to go.  They're coming out of the huddle.  \n\nThe ball is snapped back to " + madLib4 + ", who fades back and throws a long, " + madLib1 + " pass which is caught by " + madLib5 + ", who is West Point's " + madLib2 + " quarterback.  \n\nHe's in the clear, and he races over the " + madLib8 + " for a touchdown.  \n\nNo, no, wait!  \n\nThe referee is calling the play back to the 35-" + madLib9 + " line.  \n\nHe's going to penalize West Point for " + madLib14 + ".";
+
+//Ran script, prompt ok
