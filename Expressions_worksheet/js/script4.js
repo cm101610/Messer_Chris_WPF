660a51a7686b661482413ba8c3b2de7d731b064f
Chris Messer <chrismesser24@yahoo.com>
2014-04-09 22:36:49 -0400

    Added code for average
    Added additional code to get the average of the total amount spent
    between each of the 5 weeks.

--- Expressions_worksheet/js/script4.js
+++ Expressions_worksheet/js/script4.js
@@ -2,7 +2,8 @@
 
 var grocerySpend = [102.95, 126.17, 112.59, 95.18, 89.93];
 
-var total = grocerySpend [0] + grocerySpend [1] + grocerySpend [2] + grocerySpend [3] +
-grocerySpend [4];
+var total = (grocerySpend [0] + grocerySpend [1] + grocerySpend [2] + grocerySpend [3] +
+grocerySpend [4])/5;
+
 
 console.log(total);
