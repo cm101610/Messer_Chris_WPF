a2274913c1adcc42b4c13e624bbd9bea883db521
Chris Messer <cm101610@fullsail.edu>
2014-04-09 20:27:07 -0400

    Slice of Pie part 2
    Added the coding for slice of pie part 2 activity showing the different
    vars used and the console.log string to show the correct results of how
    many slices Sparky gets.

--- Expressions_worksheet/js/script3.js
+++ Expressions_worksheet/js/script3.js
@@ -3,5 +3,9 @@ var noPeople = 10;
 var noPizza = 6;
 
 var result = (noPizza * noSlices)/ noPeople;
+var remainder = 48 % 10;
 
-console.log(result);
+var string1 = "Sparky got "
+var string2 = " slices of pizza."
+
+console.log(string1 + remainder + string2);
