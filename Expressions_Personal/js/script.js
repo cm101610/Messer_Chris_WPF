b157959cd25631c1ad69a7d7c970268a8e662f9d
Chris Messer <cm101610@fullsail.edu>
2014-04-10 14:35:11 -0400

    Added second var
    added var frontLength

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,5 @@
 var frontWidth = prompt("We are going to provide you an estimate for mowing your lawn.  \n\nPlease tell us the width of the longest portion of your front lawn, in inches.");
 
 //Tested and prompt works.
+
+var frontLength = prompt("What is the length of the longest portion of your front lawn, also in inches?");
