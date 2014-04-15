f42f69d18a22a134e7372c9d5706c595dbdf0025
Chris Messer <cm101610@fullsail.edu>
2014-04-15 16:06:25 -0400

    Added the code to get results
    Added the code and messages to get end results printed to the console
    based on the entrants weight.

---
+++ Stuff Your Face 1/js/script.js
@@ -0,0 +1,10 @@
+//Determine whether an entrant qualifies based on his weight.
+
+weight = 250;
+if(weight >= 250){
+	//code performed if condition is true
+	console.log("The competitor qualifies for the heavyweight division.");
+
+}else{
+	console.log("The competitor needs to gain some weight!");
+}
