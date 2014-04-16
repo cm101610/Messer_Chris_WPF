d35aadb8bf74f9cfe32b4310951fa5b30f79dbc2
Chris Messer <cm101610@fullsail.edu>
2014-04-16 17:24:02 -0400

    Added description of first prompt
    Comment on what was planned for the first var and prompt.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,2 +1,4 @@
 //Chris Messer, April 17, 2014, Assignment: Conditionals - Personal
 //Collecting info from prompts to determine whether to open lemonade stand or not
+
+//prompt 1 asking user to input the current temperature
