2a8dd236a2123e6e32cf12afb04e576941dbff76
Chris Messer <cm101610@fullsail.edu>
2014-04-16 17:24:39 -0400

    First var and prompt
    var tempOutside = prompt("What is the current temperature outside?");

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,3 +2,4 @@
 //Collecting info from prompts to determine whether to open lemonade stand or not
 
 //prompt 1 asking user to input the current temperature
+var tempOutside = prompt("What is the current temperature outside?");
