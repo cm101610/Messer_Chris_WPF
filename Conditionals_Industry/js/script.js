8f4a9a89069ab7553b611e8b762b535124851ec6
Chris Messer <cm101610@fullsail.edu>
2014-04-17 12:54:47 -0400

    ELSE statement added
    Added the ELSE statement, testing to see if code works.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -14,3 +14,7 @@ if(clientSpend >= deluxeSpend){
 }else if (clientSpend >= standardSpend){
 	//Production can begin, but will be limited
 	console.log("Based on you budget, we can begin production, but with limited production resources.");
+}else{
+	//Sorry, your budget is below the standard production package, not recommended
+	console.log("We're sorry, but your budget is below our standard production cost.  We are more than happy to discuss some alternative options with you.");
+}
