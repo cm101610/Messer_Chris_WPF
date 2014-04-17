ed0fec8e796dfffd3a297be7e3f0484ceb95cb59
Chris Messer <cm101610@fullsail.edu>
2014-04-17 12:51:39 -0400

    Added 3 givens
    Added 3 givens for client spending, standard package and deluxe package.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,8 @@
 //Chris Messer, April 17, 2014, Assignment: Conditionals - Industry
 //Budgeting money for advertising campaign
 //Determining the amoutn of money a business is looking to spend
+
+//clientSpend prompt asking user for what they are looking to spend
+var clientSpend = 270
+var standardSpend = 250
+var deluxeSpend = 350
