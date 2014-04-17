3a6d2c9feada524ef4aba7d0d975442510e57c18
Chris Messer <chrismesser24@yahoo.com>
2014-04-17 19:45:29 -0400

    Var added
    Added the var being golfAvg = 75

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,3 +2,4 @@
 //Code will determine whether or not someone can join our golf league based upon average score per round.
 
 //Begining with golfAvg being 75 to check to see if code is working correctly.
+var golfAvg = 75
