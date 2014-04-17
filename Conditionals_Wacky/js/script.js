eedc67cf8ba29f87df9f32a3436efb5f798b4129
Chris Messer <chrismesser24@yahoo.com>
2014-04-17 19:44:50 -0400

    Added additional comment
    Added an additional comment to describe what the first portion of the
    code designates.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,2 +1,4 @@
 //Chris Messer, April 17, 2014, Assignment: Conditionals - Wacky
 //Code will determine whether or not someone can join our golf league based upon average score per round.
+
+//Begining with golfAvg being 75 to check to see if code is working correctly.
