d0a7b59ca00ecb5a2a2f9b08c79dded423bc1d06
Chris Messer <chrismesser24@yahoo.com>
2014-04-17 19:51:11 -0400

    Added ending }
    Forgot to add the ending } realized after seeing error in console.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,3 +9,4 @@ if(golfAvg <= 82){
 	console.log("THAT'S GREAT!!  WELCOME TO THE 2014 GOLF LEAGUE!!\n\nYour average score per round is suitable for entry.");
 }else{
 	console.log("THAT'S TERRIBLE!!\n\nUnfortunately, your average golf score per round is not worthy enough for our league.  Please go practice and don't come back until you're better.");
+}
