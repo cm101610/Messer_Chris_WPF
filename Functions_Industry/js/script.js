d2e64af727dbf6364b718fbf388e1a48016510ae
Chris Messer <chrismesser24@yahoo.com>
2014-05-04 14:48:17 -0400

    2nd Var
    Added the second var for cost which will alert the user to how many ads
    they select in the sales prompt and let them know that each ad is $150.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,4 @@
 
 //Begin with the function that will be repeated 3 times
 var sales = prompt("How many ads would you like to include in your monthly advertising package?");
+var cost = alert("You have selected " + sales + " ads for the monthly package.  Each ad is $150 each.");
