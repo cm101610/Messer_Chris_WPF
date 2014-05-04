b4c6a22a5d7dd5d7487a87ef1c46b8b548a58daf
Chris Messer <chrismesser24@yahoo.com>
2014-05-04 15:00:40 -0400

    reposition of if/else statement
    Changed the location of the if/else to occur right after the first
    prompt for var sales.  tested and the coding works correctly.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -4,12 +4,8 @@
 //Begin with the function that will be repeated 3 times
 function adSales(){
 	var sales = prompt("How many ads would you like to include in your monthly advertising package?");
-	var cost = alert("You have selected " + sales + " ads for the monthly package.  Each ad is $150 each.");
-	var total = (sales * 150);
-	console.log("Your total cost for the monthly production of your advertising campaign is $" + total + ".");
-}
 
-//Need validation added to determine if the customer input a number.
+	//Need validation added to determine if the customer input a number.
 	if(!isNaN(sales)){
 	}else{
 		//The user did not type in a number
@@ -17,6 +13,12 @@ function adSales(){
 		//console.log("ELSE Statement");
 		sales=prompt("You didn't enter a number to represent the number of ads you wish to add to your montly procudtion cost.\n\nPlease enter a number...");
 	}
+	//Once determined that the user entered a number, alert the user what they input and that each ad is $150
+	var cost = alert("You have selected " + sales + " ads for the monthly package.  Each ad is $150 each.");
+	var total = (sales * 150);
+	console.log("Your total cost for the monthly production of your advertising campaign is $" + total + ".");
+}
+
 //console.log(total);
 adSales();
 adSales();
