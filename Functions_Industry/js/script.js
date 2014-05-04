54cd848011e895891b6691a47808726ac14024b0
Chris Messer <chrismesser24@yahoo.com>
2014-05-04 14:47:24 -0400

    1st Var
    Added the first var for sales which will prompt the user to enter in
    how many ads per month they would like to include in their package.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,3 +2,4 @@
 //Determining the production cost base upon client input
 
 //Begin with the function that will be repeated 3 times
+var sales = prompt("How many ads would you like to include in your monthly advertising package?");
